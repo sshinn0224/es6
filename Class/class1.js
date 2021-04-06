bcc1ff85e27d755fba2bf9bb17a7427611b5f9eb
sshinn0224 <sshinn0224@gmail.com>
2021-04-06 09:15:44 +0900

    class

--- Class/class1.js
+++ Class/class1.js
@@ -1,1 +1,38 @@
 "use strict";
+
+// class Member {
+//     getName() {
+//         return "이름";
+//     }
+// };
+
+// let obj = new Member();
+
+// console.log(obj.getName());
+// console.log(obj);
+
+// class #2 expression
+// let Member = class {
+//     getName() {
+//         return "이름";
+//     }
+// };
+
+// let obj = new Member();
+
+// console.log(obj.getName());
+// console.log(obj);
+
+// class #3
+class Member {
+    setName(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+};
+
+Member.prototype.getTitle = function(){};
+console.log(typeof Member);
