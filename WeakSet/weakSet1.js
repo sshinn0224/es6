a391b0e4b6219acf695646253c892ac253a70987
sshinn0224 <sshinn0224@gmail.com>
2021-04-20 12:39:32 +0900

    weakSet

--- WeakSet/weakSet1.js
+++ WeakSet/weakSet1.js
@@ -10,3 +10,27 @@
 // } catch(e) {
 //     console.log("object 이외 지정 불가");
 // }
+
+// #2. add() : value 추가
+// const newWeakSet = new WeakSet();
+// let newString = new String("문자열");
+
+// newWeakSet.add(newString);
+
+// let obj = {sports: "스포츠"};
+// newWeakSet.add(obj);
+
+// console.log(newWeakSet);
+
+// #3.has() : value 존재 여부
+// let newString = new String("문자열");
+// const newWeakSet = new WeakSet([newString]);
+// console.log(newWeakSet.has(newString));
+
+
+// #4. delete() : 엘리먼트 삭제
+let newString = new String("문자열");
+const newWeakSet = new WeakSet([newString]);
+
+console.log(newWeakSet.delete(newString));
+console.log(newWeakSet.has(newString));
